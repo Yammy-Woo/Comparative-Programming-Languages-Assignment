2b8e2192eb9afe6b21c71efbbc09bc293231631b
wooh2 <ho.woo2@mail.dcu.ie>
2023-10-28 18:41:25 +0100

    Modified problem 60

--- ho-woo/object-orientated/euler_60_prime_pair_sets.java/euler_60_prime_pair_sets.java
+++ ho-woo/object-orientated/euler_60_prime_pair_sets.java/euler_60_prime_pair_sets.java
@@ -6,137 +6,129 @@ class Prime {
     private static ArrayList<Integer> primes = new ArrayList<Integer>();
     private static int lastPrime = 0;           // Last element in primes, which is also the largest prime in primes
 
-    public static boolean isPrime(int n) {
-        if (primes.contains(n)) { return true; }
-        
+    public static boolean isPrime(long n) {        
         if (!isPrimeByList(n)) { return false; }
         if (lastPrime <= Math.sqrt(n)) {
-            for (int i = lastPrime + 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) {
-                    return false;
-                }
+            for (long i = lastPrime + 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) { return false; }
             }
         }
-        
         return true;    // True if the given number is a prime number
     }
 
-    private static boolean isPrimeByList(int n) {
+    private static boolean isPrimeByList(long n) {
         for (int prime : primes) {   
-            if (n % prime == 0) {
-                return false;   // False if the given number is not a prime number
-            }
-            if (prime > Math.sqrt(n)) {
-                return true;
-            }
+            if (n % prime == 0) { return false; }   // False if the given number is not a prime number
+            if (prime > Math.sqrt(n)) { return true; }
         }
         return true;    // True if the given number is a prime number
     }
 
-    public static void addNextPrime() {
-        int n;
-        if (lastPrime == firstPrime) { 
-            n = lastPrime + 1;
-        }
-        else {
-            n = lastPrime + 2;
-            while (!isPrimeByList(n)) {
-                n += 2;
+    public static void generatePrimes(int limit) {
+        while (lastPrime < limit) {
+            int n;
+            if (lastPrime == firstPrime) { 
+                n = lastPrime + 1;
             }
-        }
-        lastPrime = n;
-        primes.add(lastPrime);
-    }
-
-    public static boolean checkPrimeConcat(int n1, int n2) {
-        String str1 = Integer.toString(n1);
-        String str2 = Integer.toString(n2);
-        int concat12 = Integer.parseInt(str1 + str2);
-        int concat21 = Integer.parseInt(str2 + str1);
-        
-        if (!isPrime(concat12) || !isPrime(concat21)) {
-            return false;
-        }
-        return true;
-    }
-
-    public static boolean checkSet(int prime, ArrayList<Integer> set) {
-        if (set.isEmpty()) { return true; }
-        for (int p : set) {
-            if (!checkPrimeConcat(p, prime)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /* Return the set of primes for which any two primes concatenate to produce another prime.
-       noPrime: Number of primes required to be found
-       set: The set of primes found
-       lastPrime: The last element in the set
-     */
-    public static ArrayList<Integer> primePairSet(int noPrime, ArrayList<Integer> set, int lastPrime) {
-        if (noPrime < 1) { return set; }    // Return the set if the number of primes required to be found is 0
-
-        for (int prime : primes) {
-            if (prime <= lastPrime) { continue; }   // Skip the already checked primes
-            /* Skip 2 and 5 since they always result in non primes when they are concatenated to other numbers 
-               e.g. 12, 22, 15, 25 */
-            if (prime == 2 || prime == 5) { continue; }
-
-            /* Call checkSet() to check if the prime concatenating to any prime in the set always results in a prime */
-            if (checkSet(prime, set)) {
-                /* Initialise newSet and add the prime to the set, then recursively call primePairSet() to find next prime */
-                ArrayList<Integer> newSet = new ArrayList<Integer>(set);
-                newSet.add(prime);
-                lastPrime = prime;
-                newSet = primePairSet(noPrime - 1, newSet, lastPrime);
-                /* Check if the new set's size matches the number of primes required to be found */
-                if (newSet.size() - set.size() == noPrime) {
-                    return newSet;  // Return the new set
+            else {
+                n = lastPrime + 2;
+                while (!isPrimeByList(n)) {
+                    n += 2;
                 }
             }
+            lastPrime = n;
+            primes.add(lastPrime);
         }
-        return set; // Return the original set if no matching prime is found.
     }
 
-    public static ArrayList<Integer> primePairSet(int noPrime) {
-        return primePairSet(noPrime, new ArrayList<Integer>(), 0);
-    }
-
-    public static int sumOfPrimePairSet(int noPrime) {
-        int sum = 0;
-        while (lastPrime < 10000) {
-            addNextPrime();
+    static class PrimeSet {
+        public static boolean checkPrimeConcat(int n1, int n2) {
+            String str1 = Integer.toString(n1);
+            String str2 = Integer.toString(n2);
+            long concat12 = Long.parseLong(str1 + str2);
+            long concat21 = Long.parseLong(str2 + str1);
+            
+            if (!isPrime(concat12) || !isPrime(concat21)) { return false; }
+            return true;
         }
 
-        ArrayList<Integer> set = primePairSet(noPrime);
-        if (set.size() < noPrime) {
-            System.out.println(set);
-            System.out.println("Set not found.");
-            return 0;
+        public static boolean checkSet(int prime, ArrayList<Integer> set) {
+            if (set.isEmpty()) { return true; }
+            for (int p : set) {
+                if (!checkPrimeConcat(p, prime)) { return false; }
+            }
+            return true;
         }
-        for (int prime : set) {
-            sum += prime;
+
+        /* Return the set of primes for which any two primes concatenate to produce another prime.
+        noPrime: Number of primes required to be found
+        set: The set of primes found
+        lastPrime: The last element in the set
+        */
+        public static ArrayList<Integer> primePairSet(int noPrime, ArrayList<Integer> set, int lastPrime) {
+            if (noPrime < 1) { return set; }    // Return the set if the number of primes required to be found is 0
+
+            for (int prime : primes) {
+                if (prime <= lastPrime) { continue; }   // Skip the already checked primes
+                /* Skip 2 and 5 since they always result in non primes when they are concatenated to other numbers 
+                e.g. 12, 22, 15, 25 */
+                if (prime == 2 || prime == 5) { continue; }
+
+                /* Call checkSet() to check if the prime concatenating to any prime in the set always results in a prime */
+                if (checkSet(prime, set)) {
+                    /* Initialise newSet and add the prime to the set, then recursively call primePairSet() to find next prime */
+                    ArrayList<Integer> newSet = new ArrayList<Integer>(set);
+                    newSet.add(prime);
+                    lastPrime = prime;
+                    newSet = primePairSet(noPrime - 1, newSet, lastPrime);
+                    /* Check if the new set's size matches the number of primes required to be found */
+                    if (newSet.size() - set.size() == noPrime) {
+                        return newSet;  // Return the new set
+                    }
+                }
+            }
+            return set; // Return the original set if no matching prime is found.
         }
-        return sum;
+
+        public static ArrayList<Integer> primePairSet(int noPrime) {
+            return primePairSet(noPrime, new ArrayList<Integer>(), 0);
+        }
+
+        public static int sumOfPrimePairSet(int setSize) {
+            int sum = 0;
+            int limit = 10000;
+            ArrayList<Integer> set;
+
+            generatePrimes(limit);
+    
+            set = primePairSet(setSize);
+            if (set.size() < setSize) {
+                System.out.println("Set not found within prime numbers up to " + limit + ".");
+                System.exit(1);
+            }
+
+            for (int prime : set) {
+                sum += prime;
+            }
+            return sum;
+        }    
     }
 }
 
 public class euler_60_prime_pair_sets {
-    static int noPrime = 5;    // Number of primes in the set
+    static int setSize = 5;    // Number of primes in the set
     public static void main(String[] args)
     {
         if (args.length > 0)
         {
-            noPrime = Integer.parseInt(args[0]);
-            if (noPrime < 2) 
+            setSize = Integer.parseInt(args[0]);
+            if (setSize < 2) 
             {
                 System.out.println("The set size cannot be smaller than 2.");
-                return ;
+                System.exit(1);
             }
         }
 
-        System.out.println(Prime.sumOfPrimePairSet(noPrime));
+        System.out.println(Prime.PrimeSet.sumOfPrimePairSet(setSize));
     }
 }
--- ho-woo/procedural/euler-60-prime-pair-sets.c/euler-60-prime-pair-sets.c
+++ ho-woo/procedural/euler-60-prime-pair-sets.c/euler-60-prime-pair-sets.c
@@ -1,69 +1,164 @@
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
+
+// Since the maximum limit of prime number is 10000, the maximum number of digits in a prime number is 5.
+#define MAXPRIME 10000
+#define MAXDIGIT 5    
 
 const int firstPrime = 2;
 int* primes;
-int* sizedPrimes;
-int size;
+int primeSize;
+int setSize;
 
-/* Function to check whether a given number is a prime number */
-bool isPrime(int n)
-{
-    for (int i = firstPrime; i <= n / 2; i++)
-    {
-        if (n % i == 0) 
-        {
-            return false;   // False if the given number is not a prime number
-        }
+void printArray(int size, int* array) {
+    printf("[");
+    for (int i = 0; i < size; i++) {
+        printf("%d, ", array[i]);
     }
-    return true;    // True if the given number is a prime number
+    printf("]\n");
 }
 
 /* Function to check whether a given number is a prime number by the list of prime numbers */
-bool isPrimeByList(int n)
-{
-    for (int i = 0; primes[i] <= n / 2; i++)
-    {
-        if (n % primes[i] == 0) 
-        {
-            return false;   // False if the given number is not a prime number
-        }
+bool isPrimeByList(long int n) {
+    for (int i = 0; primes[i] <= sqrt(n); i++) {
+        if (n % primes[i] == 0) { return false; } // False if the given number is not a prime number
     }
-    return true;    // True if the given number is a prime number
+    return true; // True if the given number is a prime number
 }
 
-void generatePrimes(int limit)
-{
-    sizedPrimes = malloc(sizeof(int));
-    size = 1;
-    sizedPrimes[0] = 2;
-    for (int i = 3; i < limit; i += 2) {
+/* Function to check whether a given number is a prime number */
+bool isPrime(long int n) {
+    if (!isPrimeByList(n)) { return false; }
+    
+    if (primes[primeSize - 1] <= sqrt(n)) {
+        for (long int i = firstPrime; i <= sqrt(n); i++) {
+        if (n % i == 0) { return false; } // False if the given number is not a prime number
+        }
+    }
+    return true; // True if the given number is a prime number
+}
+
+void generatePrimes() {
+    primes = malloc(sizeof(int));
+    primeSize = 1;
+    primes[0] = 2;
+    for (int i = 3; i < MAXPRIME; i += 2) {
         if (isPrimeByList(i)) {
-            size++;
-            sizedPrimes = realloc(sizedPrimes, sizeof(int) * size);
-            sizedPrimes[size - 1] = i;
+            primeSize++;
+            primes = realloc(primes, sizeof(int) * primeSize);
+            primes[primeSize - 1] = i;
         }
     }
-    primes = sizedPrimes;
 }
 
-int main(int argc, char **argv)
-{
-    int setSize = 5;        // Set default set size to 5 as required in the original question
+bool checkPrimeConcat(int n1, int n2) {
+    char* str1 = malloc(sizeof(char) * MAXDIGIT);
+    char* str2 = malloc(sizeof(char) * MAXDIGIT);
+    sprintf(str1, "%d", n1);
+    sprintf(str2, "%d", n2);
+    printf("Check %s and %s\n", str1, str2);
 
-    if (argc > 1)           // Take input from command line if a upper limit is given
-    {
+    char* concat = malloc(sizeof(char) * (strlen(str1) + strlen(str2)));
+    strcpy(concat, str1);
+    long int concat12 = atol(strcat(concat, str2));
+    
+    strcpy(concat, str2);
+    long int concat21 = atol(strcat(concat, str1));
+    
+    printf("Check %ld and %ld\n", concat12, concat21);
+    if (!isPrime(concat12) || !isPrime(concat21)) { return false; }
+    return true;
+}
+
+bool checkSet(int prime, int *set) {
+    // if (set[1] == 0) { return true; } // set contains less than two primes if the second element is zero
+    for (int i = 0; set[i] != 0; i++) {
+        if (!checkPrimeConcat(set[i], prime)) { 
+            printf("false\n");
+            return false; 
+        }
+        printf("true\n");
+    }
+    return true;
+}
+
+int* primePairSetRecur(int noPrime, int* set, int lastPrime) {
+    if (noPrime < 1) { return set; }  // Return the set if the number of primes required to be found is 0 
+    for (int i = 0; i < primeSize; i++) {
+        if (primes[i] <= lastPrime) { continue; }   // Skip the already checked primes
+        /* Skip 2 and 5 since they always result in non primes when they are
+           concatenated to other numbers e.g. 12, 22, 15, 25 */ 
+        if (primes[i] == 2 || primes[i] == 5) { continue; }
+
+        printf("%d: %d\n", noPrime, primes[i]);
+  
+        /* Call checkSet() to check if the prime concatenating to any prime in
+           the set always results in a prime */ 
+        if (checkSet(primes[i], set)) {
+            /* Initialise newSet and add the prime to the set, 
+               then recursively call primePairSet() to find next prime */ 
+            int* newSet = malloc(sizeof(set));
+            memcpy(newSet, set, sizeof(set));
+            newSet[setSize - noPrime] = primes[i];
+            printf("Before recursion: ");
+            printArray(setSize - noPrime + 1, newSet);
+            lastPrime = primes[i];
+            newSet = primePairSetRecur(noPrime - 1, newSet, lastPrime);
+            printf("After recursion: ");
+            printArray(setSize, newSet);
+            printf("Last: %d\n", newSet[setSize - (noPrime - 1)]);
+            /* Check if the new set's size matches the number of primes required to be found */ 
+            if (newSet[setSize - noPrime] != 0) {
+                return newSet;  // Return the new set
+            }
+        }
+    }
+    return set; // Return the original set if no matching prime is found.
+}
+
+int* primePairSet(int noPrime) {
+    int* set = malloc(sizeof(int) * noPrime);
+    return primePairSetRecur(noPrime, set, 0);
+}
+
+int sumOfPrimePairSet() {
+    int sum = 0;
+
+    generatePrimes();
+    
+    int* set = primePairSet(setSize);
+    printArray(setSize, set);
+
+    
+
+    if (set[setSize - 1] == 0) {
+        printf("Set not found within prime numbers up to %d.\n", MAXPRIME);
+        exit(1);
+    }
+
+    for (int i = 0; i < setSize; i++) {
+        sum += set[i];
+    }
+    return sum;
+}
+
+int main(int argc, char **argv) {
+    setSize = 4; // Set default set size to 5 as required in the original question
+
+    if (argc > 1) { // Take input from command line if a upper limit is given
         setSize = atoi(argv[1]);
-        if (setSize <= 2) 
-        {
+        if (setSize < 2) {
             printf("The set size should not be lower than 2.\n");
             return 0;
         }
     }
-
-    generatePrimes(10000);
-    for (int i = 0; i < size; i++) {
-        printf("%d\n", primes[i]);
-    }
+ 
+    printf("%s\n", isPrime(6733) ? "true" : "false");
+    printf("%s\n", checkPrimeConcat(673, 3) ? "true" : "false");
+    // printf("%d\n", sumOfPrimePairSet());
+    int temp[3] = {3, 7, 109};
+    printf("%s\n", checkSet(673, temp) ? "true" : "false");
 }
